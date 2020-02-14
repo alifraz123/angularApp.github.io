1670117a20b5c2de8515c31a3a30c1e785eeea98
yasir <yasir@naaz.com>
2020-02-14 16:13:00 +0500

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SABYASACHILikeComponent } from './sabyasachi-like/sabyasachi-like.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    SABYASACHILikeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sabyasachi-like/sabyasachi-like.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sabyasachi-like',
+  templateUrl: './sabyasachi-like.component.html',
+  styleUrls: ['./sabyasachi-like.component.css']
+})
+export class SABYASACHILikeComponent implements OnInit {
+
+  constructor() { }
+  users = [
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'},
+    {name : 'alifraz',job:'coding'}
+  ]
+
+  ngOnInit() {
+  }
+
+}
